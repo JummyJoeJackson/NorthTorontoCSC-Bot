a3f9f2260ee62cb3a2d1e4aba5801fbca49ea9b7
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2023-11-19 13:15:48 -0500

    Update responses.py
    Commented the code

--- responses.py
+++ responses.py
@@ -1,7 +1,13 @@
+#imports random library to add random chance functionality
 import random
 
+
+#takes the user's message as a parameter and returns string response
 def handle_response(message) -> str:
+    #sets variable to lowercase version of the user's message
     p_message = message.lower()
+    
+    #checks if message is valid and answers accordinly
     if p_message == "!help":
         return "`I can roll a dice, say hello and say bye`"
     elif p_message == "hello":
@@ -12,4 +18,3 @@ def handle_response(message) -> str:
         return random.randint(1, 6)
     else:
         return "I don't understand you"
-    
