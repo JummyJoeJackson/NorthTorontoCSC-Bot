1ed2dd1c28547f610b837c1ee1ac07bc0693d2f0
jakeharrison <heyJakeHarrison@gmail.com>
2023-11-21 09:37:07 -0500

    Updated code to add readability

--- bot.py
+++ bot.py
@@ -1,43 +1,38 @@
 import discord
-import responses
+from discord.ext import commands
+
+# Intents are required for the bot to work properly (DONT DELETE)
+intents = discord.Intents.all()
+
+# Includes the intents for the bot to work properly as well as the command prefix (DONT DELETE)
+client = commands.Bot(command_prefix = '?', intents=intents)
+
+#Start up code, for when the bot is ready
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}!")
+
+'''
+To add a command use the following template:
+
+@client.command() <-- This is the command decorator, needed for the bot to recognize the command
+async def <command name>(ctx, parameters): <-- This is the command name, this is what you type in discord to use the command, and the parameters are what you have to pass into the command. You will probably need to put ctx as a parameter in order to send messages. 
+    await ctx.send(<message>) <-- This is the code that runs when the command is used, ctx.send sends a message to the channel the command was used in, and the message is what is sent
+'''
 
 
-async def on_user_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(message.content)
-        if is_private:
-            await message.channel.send("Response sent to DMs")
-            await message.author.send(response) 
-        else:
-            await message.channel.send(response)
-    except Exception as e:
-        print(e)
+#Command to test if the bot is working
+#Commands can be used by doing ?ping in the discord server
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+# Command to add two numbers together
+#Commands can be used by doing ?add (number1) (number2) in the discord server
+@client.command()
+async def add(ctx, number1, number2):
+    await ctx.send(int(number1) + int(number2))
 
 
-def run_discord_bot():
-    TOKEN = "token"
-    intents = discord.Intents.all()
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f"Logged in as {client.user}!")
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        print(f"{username}: {user_message} ({channel})")
-
-        if user_message[0] == "?":
-            user_message = user_message[1:]
-            await on_user_message(message, user_message, is_private=True)
-        else:
-            await on_user_message(message, user_message, is_private=False)
-
-
-    client.run(TOKEN)
+#Enter the bot token here, in " " (DONT DELETE)
+client.run("Token")
